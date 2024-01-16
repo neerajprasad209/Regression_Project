08540eae10cc28282f0c938c260a3e3b8f00bed0
neerajprasad209 <neerajprasad.209@gmail.com>
2024-01-16 19:29:14 +0530

    AWS config File

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ import pandas as pd
 import numpy as np
 import pickle 
 
-app = Flask(__name__)
+application = Flask(__name__)
+app = application
 
 # import ridge regresor model and standard scaler pickle
 ridge_model = pickle.load(open("models/ridge.pkl",'rb'))
